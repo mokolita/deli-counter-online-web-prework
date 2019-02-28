d5097492c1f9628d96d2f2a8a8689a8b408d4dc2
Meg Galvez <meg@oakmaine.com>
2019-02-28 12:41:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,10 @@ def line(array)
   counter = 1 
   if array = 0  
     puts "The line is currently empty."
-  end 
+  
   
   else array >= 1 
     puts "The line is currently: #{counter}. #{name} "
     counter += 1 
-  end 
+   
 end
