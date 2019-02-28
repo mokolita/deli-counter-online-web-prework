5490c4bd3b98dd917e97cbf1b6366badba618113
Meg Galvez <meg@oakmaine.com>
2019-02-28 12:29:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ end
 
 def now_serving
   if(katzDeliLine>[]) 
-    return `Currently serving ${katzDeliLine.shift()}.`
+    return "Currently serving ${katzDeliLine.shift()}."
   
   else 
     return "There is nobody waiting to be served!"
@@ -17,11 +17,11 @@ def line(katzDeliLine)
 var line=[]
 if(katzDeliLine>[]) 
   for(var i=0;i<katzDeliLine.length;i++) 
-      line.push(` ${i+1}. ${katzDeliLine[i]}`)
+      line.push( ${i+1}. ${katzDeliLine[i]})
     
     return "The line is currently:" + line
 
     else 
       return "The line is currently empty."
-    end 
+    
 end
