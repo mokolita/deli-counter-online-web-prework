4f7f2087037f8a9a35feb4bf6024cf7926b8d7ec
Meg Galvez <meg@oakmaine.com>
2019-02-28 12:47:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,15 @@
 def line(array)
   
   counter = 1 
-  if array = 0  
-    puts "The line is currently empty."
-  
-  
-  else array >= 1
+   if array >= 1
     array.push do |name|
     puts "The line is currently: #{counter}. #{name} "
     counter += 1 
+  
+  
+  else array = 0  
+    puts "The line is currently empty."
+  
   end 
   end 
 end
